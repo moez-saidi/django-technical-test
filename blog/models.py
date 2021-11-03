4e3760d35535570c824525f7c2cd26841e168dd5
Edenilson Ferreira de Carvalho <edenilson.ferreira@jalasoft.com>
2021-11-03 11:29:23 -0300

    Add properties for Blog and Comment models

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,27 @@
 from django.db import models
 
-
 class Blog(models.Model):
     """
     Create blog model with corresponding requirements
     """
-    pass
+    title = models.CharField(max_length=50, blank=False)
+    description = models.TextField(max_length=50, blank=False)
+    image = models.ImageField(upload_to="blog_image/", blank=False, null=True)
 
+
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     """
     Comment blog model with corresponding requirements
     """
-    pass
+    text = models.CharField(max_length=50, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, blank=True, null=True)
+
+
+    def __str__(self):
+        return self.text
+
+
