adc7e5d40199cb9afe9e9edeeb8f51dd6cd3e328
Edenilson Ferreira de Carvalho <edenilson.ferreira@jalasoft.com>
2021-11-03 11:31:01 -0300

    Create blog and comments enpoints

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from .views import BlogAPIView, CommentAPIView
+
 app_name = "blog"
 
 urlpatterns = [
-
+    path('comment/', CommentAPIView.as_view(), name = 'comment'),
+    path('blog/', BlogAPIView.as_view(), name='blog'),
 ]
