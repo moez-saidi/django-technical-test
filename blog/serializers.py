e465a3337a645042172f91707793766ccd9172b7
Edenilson Ferreira de Carvalho <edenilson.ferreira@jalasoft.com>
2021-11-03 11:30:20 -0300

    Create serializers to validate the input/output data

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,16 +1,24 @@
 from rest_framework import serializers
 
+from .models import Blog, Comment
+
 
 class CommentSerializer(serializers.ModelSerializer):
     """
     Create comment serializer
     """
-    pass
+
+    class Meta:
+        model = Comment
+        fields = "__all__"
+        read_only_fields = ("created_at",)
 
 
 class BlogSerializer(serializers.ModelSerializer):
     """
     Create blog serializer
     """
-    pass
 
+    class Meta:
+        model = Blog
+        fields = "__all__"
