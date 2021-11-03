104bc1ee87e1c4fbb58616dd860398dafd787d25
Edenilson Ferreira de Carvalho <edenilson.ferreira@jalasoft.com>
2021-11-03 11:31:32 -0300

    Register models on admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comment, Blog
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("text", "blog", "created_at")
+    fields = ("text", "blog")
+
+class CommentAdminInline(admin.TabularInline):
+    model = Comment
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ("title", "description")
+    fields = ("title", "description",)
